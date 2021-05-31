764ce00b25fe2291b79fb4d403071808fd70941a
Peter Clark <peter@5clarks.net>
2021-05-31 16:03:18 -0500

    add cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,13 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
+  allow do
+    origins 'localhost:3001', 'amazing-engelbart-f1353f.netlify.app'
+
+    resource '*',
+      :headers => :any,
+      :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+      :max_age => 0
+  end
+end
